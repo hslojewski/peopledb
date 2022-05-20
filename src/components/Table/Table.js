8bd62dd51759452a2f0f437324d7cbe4a4e64209
Heidi Slojewski <heidi.slojewski@disney.com>
2022-05-20 00:24:52 -0700

    disable save button if note is empty

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -16,7 +16,7 @@ function Table() {
         [peopleNotes, setPeopleNotes] = useState([]),
         [editMode, setEditMode] = useState(null),
         [visibleNotes, setVisibleNotes] = useState([]),
-        [formErrors, setFormErrors] = useState({required:[],format:[]});
+        [formErrors, setFormErrors] = useState({});
 
   useEffect(() => {
     setPeopleNotesData();
@@ -57,7 +57,8 @@ function Table() {
   }
 
   var editRecord = (type, data, dataId) => {
-    if (hasFormErrors(data)) { return; }
+    debugger;
+    if (hasFormErrors(data, dataId)) { return; }
     $.ajax({
         type: "PUT",
         datatype : "application/json",
@@ -67,6 +68,7 @@ function Table() {
         dataType: "html",
         success: function(data) {
           console.log('success');
+          setEditMode(null);
           setPeopleData();
         },
         error: function(error) {
@@ -111,65 +113,39 @@ function Table() {
     });
   }
 
-  //
-
-  var enterEditMode = (e) => {
-    console.log(e);
-    console.log(people);
-    setEditMode(e.target.id);
-  };
-
-  var toggleNotes = (e) => {
-    console.log(e);
-    console.log(people);
-    var newNotes;
-    if (visibleNotes.includes(e.target.id)) {
-      newNotes = visibleNotes.filter(function(note) {
-        return note !== e.target.id
-      })
-    } else {
-      newNotes = visibleNotes;
-      newNotes.push(e.target.id);
-    }
-    setVisibleNotes([...newNotes]);
-  };
+  // Data update functions
 
   var savePeopleForm = (e) => {
-    console.log(e);
     e.preventDefault();
-    var blah = {};
+    var formFields = {};
+
     $("#form_"+e.target.id).serializeArray().forEach(item => {
       if (item.value) {
-        console.log(people);
-        // debugger;
         if (item.name === "peopleNotes") {
-          blah[item.name] = item.value.split(",");
+          formFields[item.name] = item.value.split(",");
         } else {
-          blah[item.name] = item.value;
+          formFields[item.name] = item.value;
         }
       }
-    })
-    console.log(blah);
-    editRecord("People", {fields: blah}, e.target.id);
-    setEditMode(null);
+    });
+
+    editRecord("People", {fields: formFields}, e.target.id);
+    // setEditMode(null);
   };
 
   var savePeopleNoteForm = (e) => {
-    console.log(e);
     e.preventDefault();
-    var blah = {};
+    var formFields = {};
+
     $("#form_"+e.target.id).serializeArray().forEach(item => {
-      // peopleNotes[e.target.id].peopleId;
       if (item.value) {
-        blah[item.name] = item.value;
-        blah.peopleId = peopleNotes[e.target.id].fields.peopleId;
+        formFields[item.name] = item.value;
+        formFields.peopleId = peopleNotes[e.target.id].fields.peopleId;
       }
-      console.log(people);
     })
-    console.log(blah);
-      // debugger;
-    editRecord("PeopleNotes", {fields: blah}, e.target.id);
-    setEditMode(null);
+
+    editRecord("PeopleNotes", {fields: formFields}, e.target.id);
+    // setEditMode(null);
   };
 
   var deletePerson = (e) => {
@@ -181,68 +157,72 @@ function Table() {
   };
 
   var createPerson = (e) => {
-    console.log(e);
     e.preventDefault();
-    var blah = {};
+    var formFields = {};
+
     $("#new-person").serializeArray().forEach(item => {
       if (item.value) {
-        console.log(people);
-        // debugger;
-        // if (item.name === "peopleNotes") {
-        //   blah[item.name] = item.value.split(",");
-        // } else {
-          blah[item.name] = item.value;
-        // }
+        formFields[item.name] = item.value;
       }
-    })
-    console.log(blah);
-    createRecord("People", {fields: blah});
-    // setEditMode(null);
+    });
+
+    createRecord("People", {fields: formFields});
   };
 
   var createNote = (e) => {
-    console.log(e);
     e.preventDefault();
-    var blah = {};
+    var formFields = {};
+
     $("#new-note").serializeArray().forEach(item => {
       if (item.value) {
-        console.log(people);
-        // debugger;
         if (item.name === "peopleId") {
-          blah[item.name] = [item.value];
+          formFields[item.name] = [item.value];
         } else {
-          blah[item.name] = item.value;
+          formFields[item.name] = item.value;
         }
       }
-    })
-    console.log(blah);
-    createRecord("PeopleNotes", {fields: blah});
-    // setEditMode(null);
+    });
+
+    createRecord("PeopleNotes", {fields: formFields});
   };
+
+  // Misc form functions
 
   var sort = (e) => {
-    console.log(e);
-    console.log("before:",people);
-    // debugger;
-    // if (e.target.id === "createdTime") {
-    //   debugger;
-    // }
-    var blah = people.sort((a,b)=> (a[e.target.id] - b[e.target.id] ? 1 : -1));
-    setPeople([...blah]);
-    console.log("after:",people);
+    var sortedData = people.sort((a,b)=> (a[e.target.id] - b[e.target.id] ? 1 : -1));
+    setPeople([...sortedData]);
   };
 
-  var hasFormErrors = (data) => {
-    // var requiredFields = ["name", "birthday", "zipCode"];
-    var requiredFields = ["name", "birthday"];
-    var missingFields = [];
-    var wrongFormats = [];
+  var enterEditMode = (e) => {
+    setEditMode(e.target.id);
+  };
+
+  var toggleNotes = (e) => {
+    var newNotes;
+
+    if (visibleNotes.includes(e.target.id)) {
+      newNotes = visibleNotes.filter(function(note) {
+        return note !== e.target.id
+      });
+    } else {
+      newNotes = visibleNotes;
+      newNotes.push(e.target.id);
+    }
+
+    setVisibleNotes([...newNotes]);
+  };
+
+  var hasFormErrors = (data, dataId) => {
+    var requiredFields = ["name", "birthday"],
+        missingFields = [],
+        wrongFormats = [];
+
     requiredFields.forEach(requiredField => {
-      // debugger;
       if (!Object.keys(data.fields).includes(requiredField)) {
         missingFields.push(requiredField);
       }
     });
+
     if (data.fields.zipCode && isNaN(data.fields.zipCode) && data.fields.zipCode.toString().length !== 5) {
       wrongFormats.push("zipCode");
     }
@@ -251,30 +231,51 @@ function Table() {
       $("#new-person input[type=text]").val("");
     }
 
-    setFormErrors({...formErrors, required: missingFields, format: wrongFormats});
+    if (dataId) {
+      setFormErrors({...formErrors, [dataId]: {required: missingFields, format: wrongFormats}});
+    } else {
+      setFormErrors({...formErrors, new: {required: missingFields, format: wrongFormats}});
+    }
+
+    if (missingFields.length || wrongFormats.length) {
+      return true;
+    }
+
+    // setFormErrors({...formErrors, required: missingFields, format: wrongFormats});
+  }
+
+  var onNoteChange = () => {
+    $(".note-details").each(index => {
+      var note = $(".note-details")[index];
+      if ($(note).val()) {
+        $(note.closest('#new-note')).find('.save').removeClass('disabled');
+      } else {
+        $(note.closest('#new-note')).find('.save').addClass('disabled');
+      }
+    });
   }
 
   return (
-    <div className="Table">
+    <div>
       <h4>Create New Person</h4>
       <form key="new-person" id="new-person" className="row g-3 needs-validation">
-        <div class="col-md-5">
-          <label for="name" class="form-label">Name</label>
+        <div className="col-md-5">
+          <label htmlFor="name" className="form-label">Name</label>
           <input id="name" className="col form-control col-md-4" type="text" name="name" required></input>
-          <div style={{display:formErrors.required.includes("name") ? "block" : "none"}} className="invalid-feedback">Name is required.</div>
+          <div style={{display:((formErrors.new||{}).required||[]).includes("name") ? "block" : "none"}} className="invalid-feedback">Name is required.</div>
         </div>
-        <div class="col-md-3">
-          <label for="birthday" class="form-label">Birthday</label>
+        <div className="col-md-3">
+          <label htmlFor="birthday" className="form-label">Birthday</label>
           <input id="birthday"className="col form-control col-md-4" type="text" name="birthday" placeholder="mm/dd/yyyy" required></input>
-          <div style={{display:formErrors.required.includes("birthday") ? "block" : "none"}} className="invalid-feedback" required>Birthday is required.</div>
+          <div style={{display:((formErrors.new||{}).required||[]).includes("birthday") ? "block" : "none"}} className="invalid-feedback" required>Birthday is required.</div>
         </div>
-        <div class="col-md-3">
-          <label for="zipCode" class="form-label">Zip Code</label>
+        <div className="col-md-3">
+          <label htmlFor="zipCode" className="form-label">Zip Code</label>
           <input id="zipCode"className="col form-control col-md-4" type="text" name="zipCode"></input>
-          <div style={{display:formErrors.required.includes("zipCode") ? "block" : "none"}} className="invalid-feedback">Zip Code is required.</div>
-          <div style={{display:formErrors.format.includes("zipCode") ? "block" : "none"}} className="invalid-feedback">Zip Code must be a 5 digit number.</div>
+          <div style={{display:((formErrors.new||{}).required||[]).includes("zipCode") ? "block" : "none"}} className="invalid-feedback">Zip Code is required.</div>
+          <div style={{display:((formErrors.new||{}).format||[]).includes("zipCode") ? "block" : "none"}} className="invalid-feedback">Zip Code must be a 5 digit number.</div>
         </div>
-        <div class="col-md-2">
+        <div className="col-md-2">
           <button type="submit" onClick={createPerson} className="col btn btn-primary">Save <i className="bi bi-save-fill" /></button>
         </div>
       </form>
@@ -306,7 +307,6 @@ function Table() {
           </tr>
         </thead>
           {Object.entries(people).map(([personId, person]) => {
-            console.log('formErrors:',formErrors);
             var inEditMode = editMode === person.id;
             var formId = "form_"+person.id;
             var personNotes = [];
@@ -314,11 +314,9 @@ function Table() {
               person.fields.peopleNotes.forEach(noteId => {
                 personNotes.push(peopleNotes[noteId]);
               });
-
             }
             var isNotesVisible = visibleNotes.includes(person.id);
-            console.log("inEditMode:",inEditMode);
-            console.log("isNotesVisible:",isNotesVisible);
+            // debugger;
             return (
               <tbody>
               <tr>
@@ -328,14 +326,18 @@ function Table() {
                 <td>
                   {inEditMode && <input className="form-control" form={formId} type="text" name="name" defaultValue={person.fields.name}></input>}
                   {!inEditMode && <span>{person.fields.name}</span>}
+                  <div style={{display:((formErrors[person.id]||{}).required||[]).includes("name") ? "block" : "none"}} className="invalid-feedback">Name is required.</div>
                 </td>
                 <td className="center">
                   {inEditMode && <input className="form-control" form={formId} type="text" name="birthday" defaultValue={person.fields.birthday}></input>}
                   {!inEditMode && <Moment format="MM/DD/YYYY">{person.fields.birthday}</Moment>}
+                  <div style={{display:((formErrors[person.id]||{}).required||[]).includes("birthday") ? "block" : "none"}} className="invalid-feedback" required>Birthday is required.</div>
                 </td>
                 <td className="center">
                   {inEditMode && <input className="form-control" form={formId} type="text" name="zipCode" defaultValue={person.fields.zipCode}></input>}
                   {!inEditMode && <span>{person.fields.zipCode}</span>}
+                  <div style={{display:((formErrors[person.id]||{}).required||[]).includes("zipCode") ? "block" : "none"}} className="invalid-feedback">Zip Code is required.</div>
+                  <div style={{display:((formErrors[person.id]||{}).format||[]).includes("zipCode") ? "block" : "none"}} className="invalid-feedback">Zip Code must be a 5 digit number.</div>
                 </td>
                 <td className="center">
                   <Moment format="YYYY-MM-DD HH:mm">{person.fields.createdTime}</Moment>
@@ -437,9 +439,9 @@ function Table() {
                     <form key="new-note" id="new-note" className="row">
                       <input type="hidden" name="peopleId" value={person.id}></input>
                       <span className="col col-1">New Note:</span>
-                      <input className="col form-control" type="text" name="note" placeholder="Message"></input>
+                      <input className="col form-control note-details" type="text" name="note" placeholder="Message" onChange={onNoteChange}></input>
                       <div className="col col-1">
-                        <a type="submit" onClick={createNote} className="btn btn-primary">
+                        <a type="submit" onClick={createNote} className="btn btn-primary save disabled">
                           Save <i className="col bi bi-save-fill" />
                         </a>
                       </div>
